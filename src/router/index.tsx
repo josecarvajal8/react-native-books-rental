5d21cc609755c35044a6221e54298494e954261e
Jose Carvajal <jcarvajal373@gmail.com>
2020-11-18 22:37:20 -0500

    fixing stack navigation base

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../components/home';
-import Login from '../components/login';
+import Detail from '../components/detail';
 
 const Stack = createStackNavigator();
 const Router = () => {
@@ -10,7 +10,7 @@ const Router = () => {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name={'Home'} component={Home} />
-                <Stack.Screen name={'Detail'} component={Login} />
+                <Stack.Screen name={'Detail'} component={Detail} />
             </Stack.Navigator>
         </NavigationContainer>
     )
