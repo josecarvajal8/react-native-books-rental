2966283cd2f7090fa516abbb6e2b784d48488883
Jose Carvajal <jcarvajal373@gmail.com>
2020-11-19 22:02:30 -0500

    implementing responsive using base dimensions

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { Text, Vibration, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import styles from './style';
+import getComponentStyle from '../../helpers/responsive';
+const _styles = getComponentStyle(styles);
 const Login = (props: any) => {
     const { navigation = null } = { ...props }
-    console.log('hello', props);
     return (
         <View style={styles.container}>
             <Text>{'Hello Login'}</Text>
             <TouchableOpacity onPress={() => navigation.navigate('Home')}>
                 <Text>{'sign in'}</Text>
             </TouchableOpacity>
+            <View style={_styles.box1} />
         </View >
     )
 }
--- src/components/login/style.ts
+++ src/components/login/style.ts
@@ -4,6 +4,13 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         flex: 1
+    },
+    box1: {
+        width: 50,
+        height: 50,
+        marginTop: 10,
+        borderWidth: 1,
+        borderRadius: 10
     }
 });
 
