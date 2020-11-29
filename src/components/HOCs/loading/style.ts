82f549b80b1f8d962867c0f511694cb0751b281d
Jose Carvajal <jcarvajal373@gmail.com>
2020-11-29 01:44:25 -0500

    implementing HOC for loading

---
+++ src/components/HOCs/loading/style.ts
@@ -0,0 +1,17 @@
+import { colors } from "../../../config/constants";
+
+const styles = {
+    loadingContainer: {
+        backgroundColor: colors.black,
+        opacity: 0.4,
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 100,
+    }
+}
+export default styles;
